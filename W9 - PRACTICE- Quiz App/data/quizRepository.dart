12fc0f6eb555b3dc361b47861a321dc978a0a2f8
Rith Seyhak <seyhakrith78@gmail.com>
2025-12-04 23:56:16 +0700

    Add files via upload

---
+++ W9 - PRACTICE- Quiz App/data/quizRepository.dart
@@ -0,0 +1,26 @@
+import '../model/quiz.dart';
+import '../model/question.dart';
+
+final Quiz myQuiz = Quiz(
+  title: "Flutter Quiz",
+  questions: const [
+    Question(
+      id: 'q1',
+      title: 'Who is the best teacher?',
+      choices: ['Ronan', 'Hongly', 'Leangsiv'],
+      correctChoice: 'Ronan',
+    ),
+    Question(
+      id: 'q2',
+      title: 'What is the best color?',
+      choices: ['Blue', 'Red', 'Green'],
+      correctChoice: 'Blue',
+    ),
+    Question(
+      id: 'q3',
+      title: 'What is the best course?',
+      choices: ['Java', 'C++', 'Flutter'],
+      correctChoice: 'Flutter',
+    ),
+  ],
+);
